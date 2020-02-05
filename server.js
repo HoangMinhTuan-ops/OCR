7a4bac6b8dab4b5d0d887e8c3474505dd1643fe3
HoangMinhTuan-ops <54118652+HoangMinhTuan-ops@users.noreply.github.com>
2020-02-05 20:01:04 +0100

    update server.js

--- server.js
+++ server.js
@@ -3,10 +3,11 @@ const multer = require("multer");
 const fs = require("fs");
 const app = express();
 const Tesseract = require("tesseract.js");
+const mongoose = require("mongoose");
 
 //middlewares
 app.use(express.urlencoded({extended: true}));
-app.use(express.json());
+app.use(express.json({extended: false}));
 
 const PORT = 5000 || process.env.PORT;
 
@@ -40,14 +41,28 @@ app.post("/upload", (req, res) => {
 var image1 = fs.readFileSync(__dirname + '/images/ocr wiki.png', {encoding: null});
 console.log(image1);
 
-Tesseract.recognize(image1, "eng").then((result) => {
-    console.log(result);
+Tesseract.recognize(image1, "eng").then((output) => {
+    console.log(output);
+
+//connection Database 
+const MongoClient = require('mongodb').MongoClient;
+const uri = "mongodb+srv://abc123:Test12345@mydb-fluxr.mongodb.net/MyDB";
+//connect to the mongo client
+const client = new MongoClient(uri, { useUnifiedTopology: true }, { useNewUrlParser: true });
+
+client.connect(err => {
+    const collection = 
+    client.db("test").collection("Content");
+    console.log("connected!");
     
+//insert one input    
+    collection.insertOne(output, function(err, res) {
+    console.log("data inserted!");
+    });
+    client.close();
 });
-
+});
 //Server starts
-
-
 app.listen(PORT, () => {
     console.log(`Server running on PORT ${PORT}`);
 });
